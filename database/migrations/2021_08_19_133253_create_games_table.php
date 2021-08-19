601b1ea0855573a8159c6dd2c7eb44a20d65adbf
titi222 <tida.daffe@3wa.io>
2021-08-19 14:58:42 +0000

    modification de tida

---
+++ database/migrations/2021_08_19_133253_create_games_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->float('mark');
+            $table->string('image');
+            $table->text('description');
+            $table->foreign('studio_id')->on('studios')->references('id');
+            $table->float('price');
+            $table->date('release_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
---
+++ database/migrations/2021_08_19_140855_create_categorie_game_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategorieGameTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categorie_game', function (Blueprint $table) {
+            $table->foreign('game_id')->on('game')->references('id');
+            $table->foreign('category_id')->on('categories')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categorie_game');
+    }
+}
