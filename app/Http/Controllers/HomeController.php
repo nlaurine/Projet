6520b791dd13749ccf00dc397608f728f01fff55
Vickou94 <victor.noel@outlook.fr>
2021-08-20 14:18:48 +0000

    homepage

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Game;
+use Illuminate\Support\Facades\DB;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $latestGames = Game::latest()->take(5)->get();
+
+            
+        $bestGames = Game::find(1);
+        
+        return view('homepage', [
+            'latestGames' => $latestGames,
+            'bestGames' => $bestGames
+        ]);
+    }
+}
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Démo Laravel - @yield('title')</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="{{ route('homepage') }}">Laravel</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="{{ route('homepage') }}">Accueil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Liste des jeux</a>
+                        </li>
+                        @auth
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Déconnexion</a>
+                            </li>
+                        @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Inscription</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Connexion</a>
+                            </li>
+                        @endauth
+                    </ul>
+                    @auth
+                        <span class="navbar-text">Bienvenue {{ auth()->user()->name }} !</span>
+                    @endauth
+                </div>
+            </div>
+        </nav>
+    </header>
+    
+    <main class="container my-2">
+        @yield('content')
+    </main>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+</body>
+</html>
